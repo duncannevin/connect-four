48baf66383bd6337bcfd93b937955415fd7f330b
Duncan Nevin <duncan.nevin@gmail.com>
2016-10-16 15:00:44 -0400

    User interface is working.

--- js/action.js
+++ js/action.js
@@ -1,4 +1,4 @@
-window.playBoard = [['-', '-', '-', '-', '-', '-', '-'],
+   let playBoard = [['-', '-', '-', '-', '-', '-', '-'],
 			 		['-', '-', '-', '-', '-', '-', '-'],
 			 		['-', '-', '-', '-', '-', '-', '-'],
 			 		['-', '-', '-', '-', '-', '-', '-'],
@@ -17,12 +17,10 @@ let Play = (board) => {
 
 	this.boardBuilder = () => {
 
-		let count = 1;//gives each square a unique number.
-
 		for(let i = 0; i <= 5; i++){
 			this.board.append('<div class="row r' + i + '"></div>');
-			for(var j = 0; j <= 6; j++){
-				$('.r' + i).append('<div class="square s' + i + j + '"><div class="circle"></div></div>');
+			for(let j = 0; j <= 6; j++){
+				$('.r' + i).append('<div class="square s' + i + j + '"><div class="circle c' + i + j + '"></div></div>');
 				if(i === 0){
 					//adding droppable to all the first rows squares...
 					$('.s' + i + j).droppable({
@@ -31,34 +29,54 @@ let Play = (board) => {
 						}
 					});
 				}
-				count++;
 			}
-		}	
+		}
+		this.board.prepend('<div class="row invisible"></div>');
+	
 	};
-		//function that appends the tokens to board. which is which token is to be added...
+	//function that appends the tokens to board. which is which token is to be added...
 	this.appendTokens = (which) => {
-
+		//object conataining the token divs...
 		let tokens = {
 			red: '<div class="token red"></div>',
 			blue: '<div class="token blue"></div>'
 		};
-
+		//function to control the revert for draggable...
+		let controlDrag = (who) => {
+			
+		};	
+		//if null is passed in append both sets of tokens...
 		if(which === null){
 			_.each(tokens, (node) => {
 				this.board.append(node);
 			});
 		}else{
+			//else append the color that was passed in...
 			this.board.append(tokens[which]);
 		}	
-		$('.token').draggable({
-			revert: 'invalid',
-		});	
+		//initially sets the first player as the only token then is draggable...
+		this.controlDrag();	
 	};	
 	//function fills in the board and calls the validator...
 	this.updateBoard = (player, position) => {
+		//adds correct value to the playboard...
 		playBoard[position[0]][position[1]] = player;
-		console.log(playBoard);
+
+		//call the validator to check the game status...
+
 	};	
+	//function that controls if a token is daraggable...
+	this.controlDrag = () => {
+		//determine which class should be disabled...
+		let disable = turn === 'red' ? 'blue' : 'red';
+		//disable draggable...
+		$('.' + disable).draggable({disabled: true});
+		//removes draggable from player...	
+		$('.' + turn).draggable({
+			disabled: false,
+			revert: 'invalid'
+		});
+	};
 	//function gets finds the last available div in the column...
 	this.getLast = (start) => {
 		//helper function to send updateBoard the correct info...
@@ -67,8 +85,8 @@ let Play = (board) => {
 		};
 
 		let i = 0;
-
-		for(i; i <= 5; i++){
+		//iterates through the play board and looks for the first available spot below dropped token...
+		for(i; i < playBoard.length; i++){
 			if(playBoard[i][start[1]] !== '-'){
 				//updates gameboard with data...
 				update([i - 1, start[1]]);
@@ -77,37 +95,45 @@ let Play = (board) => {
 		}
 		//updates gameboard with data...
 		update([i - 1, start[1]]);
+		//returns the available spaces coordinates...
 		return [i - 1, start[1]];
+	};
+	//function that resets everything on the board after the animation is finished...
+	this.reset = (div) => {
+		//changes the circles background color to the appropriate color so that the token can be appended...
+		$('.c' + div.join('')).addClass('back-ground-' + turn);
+		//resets whos turn it is...
+		turn = turn === 'red' ? 'blue' : 'red';
+		//reset draggable...
+		this.controlDrag();
 	};	 	
-
+	//function handles a token drop once it is in the droppable div...
 	this.dropToken = (dis, ev) => {
-		//grabs to correct data for the first square, essentially where the token was placed...
+		//grabs the correct data for the first square, essentially which div the token was placed...
 		let dropPoint = dis.classList[1].split('').splice(1).map(Number);
 		//get the bottom most available div...
 		let bottomDiv = this.getLast(dropPoint);
-		// grabs the location for the start div...
+		// grabs the position of the start div...
 		let begin = $('.s' + dropPoint.join('')).position();
 		//left didn't quite line up correctly...
 		begin.left = begin.left + 5;
-		// grabs the location of the last div...
-		let end = $('.s' + bottomDiv.join('')).position();//will need to alter this later....
-		//function that resets everything on the board after the animation is finished...
-		let reset = () => {
-			//changing background color of end to appropriate color...
-			$('.s5' + dropPoint[1]).css({'background-color': turn});
-			//resets whos turn it is...
-			turn = turn === 'red' ? 'blue' : 'red';
-		};
+		// grabs the position of the bottom most available div...
+		let end = $('.s' + bottomDiv.join('')).position();
 		//animates the token dropping...
 		(function recur (){	
+			//changes the location using jquery css...
 			$('.' + turn).css({top: begin.top, left: begin.left});
-			
+			//base case...
 			if(begin.top === end.top){
+				//adds new token for the next round...
 				this.appendTokens(turn);
-				reset();
+				//resets board...
+				this.reset(bottomDiv.slice());
 				return;	
-			}	
+			}
+			//adds one to the top position each iteration...	
 			begin.top += 1;
+			//controls time between iteration...
 			setTimeout(recur, 3);
 		})()
 	};
