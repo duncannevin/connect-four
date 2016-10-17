c829dacb64694e5c73c34afa5c1788d67cc07d22
Duncan Nevin <duncan.nevin@gmail.com>
2016-10-16 23:12:08 -0400

    Had to re-write the validator, works now.

--- js/validate.js
+++ js/validate.js
@@ -1,94 +1,136 @@
-var validate = function(board){
-	//write a function that will take an array of unknown length
-	var check = function(reorgBoard){
-		//declare a variable called count, start the value at 0...
-		var count = {
-			y: 0,
-			r: 0,
-			totalCount: 0,
-			winner: false
-		};
-		reorgBoard.forEach((arr, ind, coll)=>{
-			arr.forEach((val, ind, coll2)=>{
-				if(val === 'Y'){
-					count.y++;
-					count.r = 0;
-					count.totalCount++;
-				}else if(val === 'R'){
-					count.r++;
-					count.y = 0;
-					count.totalCount++;
-				}else{
-					count.y = 0;
-					count.r = 0;
-				}
-				if(count.r === 4 || count.y === 4){
-					count.winner = count.y === 4 ? 'Y' : 'R';
-					coll.length = 0;
-					coll2.length = 0;
-				}
-			});
-			count.r = 0;
-			count.y = 0;
-		});
-		if(count.winner){
-			return count.winner;
+let validate = function(inB) {
+
+	let board = inB.slice(0);
+
+	let r = 0;
+
+	let b = 0;
+
+	let winner = false;
+
+	//utility function that checks the value and updates the correct values...
+	let checker = (val) => {
+
+		if(val !== '-'){
+			if(val === 'B'){
+				b++;
+				r = 0;
+			}
+			if(val === 'R'){
+				r++;
+				b = 0;
+			}
+			if(r === 4 || b === 4){
+				winner = r > b ? 'R' : 'B';
+			}
 		}else{
-			return count.totalCount;
-		}	
-	};
-	//write a function that converts all the vertical columns into horizontal sub arrays...
-	var convertColumns = function(board){
-		//declare a variable and assign it an empty string...
-		var solution = [];
-		// board[0].forEach((val, ind, coll)=>{solution.push([])});
-		for(var i = 0; i < board.length; i++){
-			for(var j = 0; j < board[i].length; j++){
-			if(solution[j] === undefined){
-				solution.push([]);
-			}
-				solution[j].push(board[i][j]);
-			}
+			r = 0;
+			b = 0;
 		}
-		// return check(solution);
-		return check(solution);	
 	};
-	//write a function that converts diagnols into horizontal arrays...
-	var convertDiags = function(board){
-		//declare acounter variable in order to add the correct amount of extra indexes to the front of the
-		//array...
-		var count = 6;
-		//declare a variable and assign it sliced board...
-		//implement map on the board...
-		var shiftArray = board.slice().map((arr, ind, coll)=>{
-			//use a for loop to add a '-' to the front half of the array...
-			for(var i = 1; i <= count; i++){
-				arr.splice(0, 0, '-')
+
+	//a function that iterates over the board from left to righ checking for winners...
+	let horizontal = (board) => {
+		_.each(board, (arr) => {
+			_.each(arr, (val) => {
+				//call checker...
+				checker(val);
+			});
+		});
+		return winner;
+	};
+
+	//add a function that will check all the columns for winners...
+	let columns = (board) => {
+		//declare a varaible and assigin it zero, this will be to track which column we are checking...
+		let columnInd = 0;
+		//write a recursive function that will iterate through each sub array...
+		(function recur(){
+			//write a base cast that checks if ind === 6, then return....
+			if(columnInd === board[0].length)return;
+			//iterate over the arrays checking if ind in each array...
+			_.each(board, (arr, ind, coll) => {
+				checker(board[ind][columnInd]);
+			});
+			columnInd++;
+			recur();
+		})()
+
+		return winner;
+	};
+
+	// //add a method that will add the appropriate extra indexes to each sub array, does both diag directions at one time
+	// //and compines them into on bigger array...
+	let diagnols = function(board){
+		let count = 6;
+		let bLeft = [];
+		let bRight = [];
+		let square = (arr) => {
+			return _.map(arr, (arr, ind, coll) => {
+					for(let i = arr.length; i < coll[0].length; i++){
+						arr.push('-')
+					}
+					return arr;
+			});
+		};		 
+		// using each to iterate the subarrays, will push the outcome to the appropriate array... 
+		_.each(board.slice(0), (arr, ind, coll)=>{
+			//declaring a function to iterate over sub array...
+			let iterate = (z) => {
+				let f = z;
+				//use a for loop to add the spaces...
+				for(let i = 0; i < count; i++){
+					f.splice(0, 0, '-');
+				}
+				return f;
 			}
 			count--;
-			return arr;
-		});
-		//return shiftarray, converted to using convertColumns....
-		return convertColumns(shiftArray);
+			bLeft.push(iterate(arr.slice()));
+			bRight.push(iterate(arr.slice().reverse()));
+		});	
+		//comgining both diagnol boards into one...
+		let combined = _.reduce(square(bRight), (both, arr, ind, coll) => {
+			both.push(arr);
+			return both;
+		}, square(bLeft));
+		//call and return columns on comined
+		return columns(combined);
 	};
-	//declare a variable that contains an array contianing each possible directions...
-	//I want to loop through all the possible winning patterns, if one of them mins I want to return and end the function call...
 
-	var funcs = [check/*horizontal*/, convertColumns/*vertical*/, convertDiags/*diagright*/, convertDiags/*diagleft*/];
-	var winOrNot;
-	// // implement a forEach loop that loops through the functions, passing in the appropriate argument...
-	funcs.forEach((func, ind, coll)=>{
-		var runIt = ind < coll.length-1 ? func(board) : func(board.map((arr)=>{return arr.reverse()}));
-		if(typeof runIt !== 'number'){
-			winOrNot = runIt;
-			coll.length = 0;
-		}else{
-			winOrNot = runIt;
-		}	
-	});
-	if(typeof winOrNot !== 'number'){
-		return winOrNot;
-	}else{
-		return winOrNot >= 42 ? 'draw' : 'in progress';
+	if(horizontal(board)){
+		return winner;
+	}else if(columns(board)){
+		return winner;
+	}else if(diagnols(board)){
+		return winner;
 	}
-}; 
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
